217246cbc6c8f0ccff2f00ac367f2a3771145bfa
iaroki <iaroki@protonmail.com>
2021-04-25 19:20:04 +0300

    add YAML config

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module kafka-producer
 
 go 1.16
 
-require github.com/confluentinc/confluent-kafka-go v1.6.1
+require (
+	github.com/confluentinc/confluent-kafka-go v1.6.1
+	gopkg.in/yaml.v2 v2.4.0
+)
--- main.go
+++ main.go
@@ -3,30 +3,57 @@ package main
 import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
 )
 
+type Config struct {
+	BootstrapServers      string `yaml:"bootstrap.servers"`
+	SaslMechanism         string `yaml:"sasl.mechanism"`
+	SecurityProtocol      string `yaml:"security.protocol"`
+	SslTruststoreLocation string `yaml:"ssl.truststore.location"`
+	KafkaUsername         string `yaml:"kafka.username"`
+	KafkaPassword         string `yaml:"kafka.password"`
+	KafkaConsumerGroup    string `yaml:"kafka.consumer.group"`
+	AutoOffsetReset       string `yaml:"auto.offset.reset"`
+}
+
+func getConfig(configFilePath string) Config {
+
+	yamlFile, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		fmt.Printf("Error reading YAML file: %s\n", err)
+	}
+
+	var brokerConfig Config
+
+	err = yaml.Unmarshal(yamlFile, &brokerConfig)
+	if err != nil {
+		fmt.Printf("Error parsing YAML file: %s\n", err)
+	}
+
+	return brokerConfig
+
+}
+
 func main() {
 
-	kafkaBrokers := ""
-	kafkaSaslMechanisms := ""
-	kafkaSecurityProtocol := ""
-	kafkaUsername := ""
-	kafkaPassword := ""
-	kafkaConsumerGroup := ""
+	brokerConfig := getConfig("config.yaml")
 
 	topics := []string{
 		"userEvents_gen2",
 	}
 
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": kafkaBrokers,
-		"sasl.mechanisms":   kafkaSaslMechanisms,
-		"security.protocol": kafkaSecurityProtocol,
-		"sasl.username":     kafkaUsername,
-		"sasl.password":     kafkaPassword,
-		"group.id":          kafkaConsumerGroup,
-		"auto.offset.reset": "smallest"})
+		"bootstrap.servers": brokerConfig.BootstrapServers,
+		"sasl.mechanism":    brokerConfig.SaslMechanism,
+		"security.protocol": brokerConfig.SecurityProtocol,
+		"ssl.ca.location":   brokerConfig.SslTruststoreLocation,
+		"sasl.username":     brokerConfig.KafkaUsername,
+		"sasl.password":     brokerConfig.KafkaPassword,
+		"group.id":          brokerConfig.KafkaConsumerGroup,
+		"auto.offset.reset": brokerConfig.AutoOffsetReset})
 	if err != nil {
 		fmt.Println("ERROR: ", err)
 		os.Exit(333)
