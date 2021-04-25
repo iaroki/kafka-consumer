1cae6c0f994415783e3e0baef102dc39e66ea1d3
iaroki <iaroki@protonmail.com>
2021-04-25 19:58:20 +0300

    add kafka client

---
+++ kafka.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"os"
+)
+
+func getConsumerClient(consumerConfig Config) *kafka.Consumer {
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": consumerConfig.BootstrapServers,
+		"sasl.mechanism":    consumerConfig.SaslMechanism,
+		"security.protocol": consumerConfig.SecurityProtocol,
+		"ssl.ca.location":   consumerConfig.SslTruststoreLocation,
+		"sasl.username":     consumerConfig.KafkaUsername,
+		"sasl.password":     consumerConfig.KafkaPassword,
+		"group.id":          consumerConfig.KafkaConsumerGroup,
+		"auto.offset.reset": consumerConfig.AutoOffsetReset})
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		os.Exit(333)
+	}
+
+	return consumer
+
+}
+
+func consumeMessages(consumerClient *kafka.Consumer, topic []string) {
+	//topics := []string{
+	//	consumerConfig.KafkaTopic,
+	//}
+	err := consumerClient.SubscribeTopics(topic, nil)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		os.Exit(333)
+	}
+
+	run := true
+
+	for run == true {
+		ev := consumerClient.Poll(0)
+		switch e := ev.(type) {
+		case *kafka.Message:
+			fmt.Println(string(e.Value))
+		case kafka.Error:
+			fmt.Println("ERROR: ", e)
+			run = false
+		}
+	}
+
+	err = consumerClient.Close()
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+}
